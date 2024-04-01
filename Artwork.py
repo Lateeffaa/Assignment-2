2bef9478d844778cbc4cfe40f988b79764155847
Lateeffaa <159874857+Lateeffaa@users.noreply.github.com>
2024-04-02 01:13:10 +0400

    Update Artwork.py

--- Artwork.py
+++ Artwork.py
@@ -1,1 +1,15 @@
+class Artwork:
+  def __init__(self, title, artist, date_of_creation, historical_significance):
+    self._title = title
+    self.__artist = artist
+    self.__date_of_creation = date_of_creation
+    self.__historical_significance = historical_significance
+    self.__exhibition_location = exhibition_location
 
+class Exhibition:
+  def __init__(self, name, exhibition_location, exhibition_duration):
+    self.name = name
+    self.exhibition_location = exhibition_location
+    self.exhibition_duration = exhibition_duration
+    self.artwork = []
+    
