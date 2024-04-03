c7b40c325c2faa2869f7eb6e4f01d7faa1347686
Lateeffaa <159874857+Lateeffaa@users.noreply.github.com>
2024-04-03 15:10:46 +0400

    Update Management.py

--- Management.py
+++ Management.py
@@ -1,65 +1,36 @@
-class Management:
+class Management(Person):
     """
-    Class representing an employee in the management team of the museum
+    Class representing an employee in the management team of the museum inheriting attributes from Person class
     """
 
-    def __init__(self, fName, lName, age, gender, phoneNum, employeeID, department, position):
-        # Initialization of attributes
-        self._fName = fName
-        self._lName = lName
-        self._age = age
-        self._gender = gender
-        self._phoneNum = phoneNum
-        self.__employeeID = employeeID
+    def __init__(self, f_name, l_name, gender, phone_num, employee_id, department, position):
+        super().__init__(f_name, l_name, gender, phone_num)  # Initializing the parent class with common person attributes
+        # Initializing attributes of the management employee
+        self.__employee_id = employee_id
         self.__department = department
         self.__position = position
 
-    # Setter and getter methods for attributes
-    def setFName(self, fName):
-        self._fName = fName
-    def getFName(self):
-        return self._fName
-
-    def setLName(self, lName):
-        self._lName = lName
-    def getLName(self):
-        return self._lName
-
-    def setAge(self, age):
-        self._age = age
-    def getAge(self):
-        return self._age
-
-    def setGender(self, gender):
-        self._gender = gender
-    def getGender(self):
-        return self._gender
-
-    def setPhoneNum(self, phoneNum):
-        self._phoneNum = phoneNum
-    def getPhoneNum(self):
-        return self._phoneNum
-
-    def setEmployeeID(self, employeeID):
-        self.__employeeID = employeeID
+        # Setter and getter for management employee attributes
+    def setEmployeeID(self, employee_id):
+        self.__employee_id = employee_id
     def getEmployeeID(self):
-        return self.__employeeID
+        return self.__employee_id
 
     def setDepartment(self, department):
         self.__department = department
     def getDepartment(self):
         return self.__department
 
-    def setPosition(self, position):
+     def setPosition(self, position):
         self.__position = position
     def getPosition(self):
         return self.__position
 
     def displayManagementInfo(self): # Display data about the management employee
         print("Employee Details:")
-        print("Name:", self.getFName(), self.getLName())
-        print("Gender:", self.getGender())
-        print("Phone Number:", self.getPhoneNum())
+        print("Name:", self.get_f_name(), self.get_l_name())
+        print("Gender:", self.get_gender())
+        print("Phone Number:", self.get_phone_num())
         print("Employee ID:", self.getEmployeeID())
         print("Department:", self.getDepartment())
         print("Position:", self.getPosition())
