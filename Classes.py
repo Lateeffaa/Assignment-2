fe4e300639078bfd3d9401884ff7dbd18fe2356f
Lateeffaa <159874857+Lateeffaa@users.noreply.github.com>
2024-04-03 17:27:22 +0400

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -1,8 +1,14 @@
+from enum import Enum
+from datetime import datetime
+
+
 class Artwork:
     """
     Class representing the artwork at the museum.
     """
-    def __init__(self, title, artist, date_of_creation, historical_significance, exhibition_location):  # Initialize Artwork object
+
+    def __init__(self, title, artist, date_of_creation, historical_significance,
+                 exhibition_location):  # Initialize Artwork object
         self._title = title
         self.__artist = artist
         self.__date_of_creation = date_of_creation
@@ -15,17 +21,21 @@ class Exhibition:
     Class representing an exhibition at the museum.
     Aggregation relation with Artwork class.
     """
-    def __init__(self, name, exhibition_location, exhibition_duration):  # Initialize Exhibition object
+
+    def __init__(self, name, exhibition_location, exhibition_duration, is_special_event=False,
+                 special_event_ticket_price=None):  # Initialize Exhibition object
         self.name = name
         self.exhibition_location = exhibition_location
         self.exhibition_duration = exhibition_duration
+        self.is_special_event = is_special_event
+        self.special_event_ticket_price = special_event_ticket_price
         self.artworks = []  # Empty list of artworks
 
     def addArtwork(self, artwork):  # Add artwork to the exhibition
         if artwork not in self.artworks:
             self.artworks.append(artwork)
         else:
-            print(f"The artwork '{artwork}' is already in the exhibition.")
+            raise ValueError(f"The artwork '{artwork.title}' is already in the exhibition.")
 
     def removeArtwork(self, artwork):  # Remove artwork from the exhibition
         try:
@@ -42,9 +52,8 @@ class Exhibition:
     def displayInfo(self):  # Display data about exhibition and the artworks
         print(f"Exhibition: {self.name}")
         for artwork in self.artworks:
-            print(f"Artwork: {self.artworks.index(artwork) + 1}. {artwork._title}, Artist: {artwork.__artist}, Date: {artwork.__creation_date}")
-
-
+            print(
+                f"Artwork: {self.artworks.index(artwork) + 1}. {artwork._title}, Artist: {artwork.__artist}, Date: {artwork.__creation_date}")
 
 
 class Person:
@@ -52,34 +61,45 @@ class Person:
     Class to represent a person
     (This class is the parent class for Management and Visitor classes)
     """
-    def __init__(self, f_name, l_name, gender, phone_num):
+
+    def __init__(self, f_name, l_name, age, gender, phone_num):
         # Initialization of person attributes
         self._f_name = f_name
         self._l_name = l_name
+        self._age = age
         self._gender = gender
         self._phone_num = phone_num
 
     # Setter and getter for person attributes
     def set_f_name(self, f_name):
         self._f_name = f_name
+
     def get_f_name(self):
         return self._f_name
 
     def set_l_name(self, l_name):
         self._l_name = l_name
+
     def get_l_name(self):
         return self._l_name
 
+    def set_age(self, age):
+        self._age = age
+
+    def get_age(self):
+        return self._age
+
     def set_gender(self, gender):
         self._gender = gender
+
     def get_gender(self):
         return self._gender
 
     def set_phone_num(self, phone_num):
         self._phone_num = phone_num
+
     def get_phone_num(self):
         return self._phone_num
-
 
 
 class Visitor(Person):
@@ -87,39 +107,30 @@ class Visitor(Person):
     Class representing a visitor inheriting from the Person class
     This class has an aggregation relationship with the Ticket class
     """
-    def __init__(self, f_name, l_name, gender, phone_num, age, nationality, visitorID, email):
-        super().__init__(f_name, l_name, gender,phone_num)  # Initializing the parent class with common person attributes
+
+    def __init__(self, f_name, l_name, age, gender, phone_num, nationality, visitorID, email):
+        super().__init__(f_name, l_name, age, gender,
+                         phone_num)  # Initializing the parent class with common person attributes
         # Initializing attributes of the visitor
         self.__nationality = nationality
         self.__visitorID = visitorID
         self.__email = email
         self._tickets = []  # Stores the Ticket objects since there is an aggregation relationship
 
-    # Setter and getter for visitor attributes
-    def setNationality(self, nationality):
-        self.__nationality = nationality
-    def getNationality(self):
-        return self.__nationality
-
-    def setVisitorID(self, visitorID):
-        self.__visitorID = visitorID
-    def getVisitorID(self):
-        return self.__visitorID
-
-    def setEmail(self, email):
-        self.__email = email
-    def getEmail(self):
-        return self.__email
+    def purchaseTicket(self, numTickets, ticketType, isGroup=False,
+                       isSpecialEvent=False):  # Method for online ticket purchase
+        ticket = TicketandPricing(numTickets, ticketType, isGroup, isSpecialEvent)  # Create a new Ticket object
+        self.addTicket(ticket)  # Add the ticket to the visitor's list of tickets
 
     def displayVisitorInfo(self):  # Display data about the visitor
         print("Visitor Information:")
         print("Name:", self.get_f_name(), self.get_l_name())
-        print("Age:", self.getAge())
+        print("Age:", self.get_age())
         print("Gender:", self.get_gender())
         print("Phone Number:", self.get_phone_num())
-        print("Nationality:", self.getNationality())
-        print("Visitor ID:", self.getVisitorID())
-        print("Email:", self.getEmail())
+        print("Nationality:", self.__nationality)
+        print("Visitor ID:", self.__visitorID)
+        print("Email:", self.__email)
         print()
 
     def addTicket(self, ticket):  # Add ticket to visitor's list of tickets
@@ -129,48 +140,66 @@ class Visitor(Person):
         return self._ticket[:]
 
 
-
-
-
 class Management(Person):
     """
     Class representing an employee in the management team of the museum inheriting attributes from Person class
     (This is a child class of the Person class)
     """
-    def __init__(self, f_name, l_name, gender, phone_num, employee_id, department, position):
-        super().__init__(f_name, l_name, gender, phone_num)  # Initializing the parent class with common person attributes
+
+    def __init__(self, f_name, l_name, age, gender, phone_num, employee_id, department, position):
+        super().__init__(f_name, l_name, age, gender,
+                         phone_num)  # Initializing the parent class with common person attributes
         # Initializing attributes of the management employee
         self.__employee_id = employee_id
         self.__department = department
         self.__position = position
 
-        # Setter and getter for management employee attributes
-    def setEmployeeID(self, employee_id):
-        self.__employee_id = employee_id
-    def getEmployeeID(self):
-        return self.__employee_id
-
-    def setDepartment(self, department):
-        self.__department = department
-    def getDepartment(self):
-        return self.__department
-
-    def setPosition(self, position):
-        self.__position = position
-    def getPosition(self):
-        return self.__position
-
     def displayManagementInfo(self):  # Display data about the management employee
         print("Employee Information:")
         print("Name:", self.get_f_name(), self.get_l_name())
-        print("Age:", self.getAge())
+        print("Age:", self.get_age())
         print("Gender:", self.get_gender())
         print("Phone Number:", self.get_phone_num())
-        print("Employee ID:", self.getEmployeeID())
-        print("Department:", self.getDepartment())
-        print("Position:", self.getPosition())
+        print("Employee ID:", self.__employee_id)
+        print("Department:", self.__department)
+        print("Position:", self.__position)
         print()
 
 
+class VisitorsClassification(Enum):
+    """
+    This class represents the Visitors Classification enumeration
+    """
+    A = "Adults"
+    C = "Children"
+    STUDENT = "Students"
+    T = "Teachers"
+    SENIOR = "Seniors"
+
 
 class TicketandPricing:
+    """
+    This class represents the type of ticket purchased by the visitor
+    This class is an aggregation relation with the Visitor class
+    """
+
+    def __init__(self, numTickets, ticketType, isGroup=False,
+                 isSpecialEvent=False):  # Initialize attributes of the ticket
+        self.numTickets = numTickets  # Stores the number of tickets
+        self.ticketType = ticketType  # Stores the visitor classification for the ticket
+        self.isGroup = isGroup  # Indicates if ticket is purchased as a group
+        self.isSpecialEvent = isSpecialEvent  # Indicates if ticket is for a special event
+
+    def calculatePrice(self):  # Calculate total price of tickets based on visitor classification and number of tickets
+        ticketPrice = 63.0  # Base price
+        if self.ticketType in [VisitorsClassification.C, VisitorsClassification.STUDENT, VisitorsClassification.T,
+                               VisitorsClassification.SENIOR]:  # Check certain categories eligible for free tickets
+            return 0.0  # Free ticket for certain visitor types
+        totalPrice = self.numTickets * ticketPrice  # Normal price calculation
+        if self.isGroup:
+            totalPrice *= 0.5  # 50% discount for groups
+        totalPrice += totalPrice * 0.05  # 5% VAT
+        return totalPrice
+
+    def isFree(self):  # Check if ticket is free
+        return self.calculatePrice() == 0.0
